53eca3c820c5d0def6f709f89ecf0e264fe7169c
netakoi <choi774@purdue.edu>
2025-12-27 13:21:41 -0500

    Update Autoencoder.py

--- source/less_leg_walking_1/less_leg_walking_1/tasks/direct/less_leg_walking_1/Autoencoder.py
+++ source/less_leg_walking_1/less_leg_walking_1/tasks/direct/less_leg_walking_1/Autoencoder.py
@@ -15,8 +15,7 @@ class Encoder_walk(nn.Module):
             nn.Tanh(),
             nn.Linear(hidden_dim*2, hidden_dim),
             nn.Tanh(),
-            nn.Linear(hidden_dim, observable_dim),
-            nn.Tanh()
+            nn.Linear(hidden_dim, observable_dim)
         )
     
     def forward(self, x):
@@ -34,33 +33,40 @@ class Decoder(nn.Module):
 class KoopmanAutoencoder_walk(nn.Module):
     def __init__(self, state_dim, hidden_dim, observable_dim,device):
         super().__init__()
-        self.encoder = Encoder(state_dim, hidden_dim, observable_dim)
+        self.encoder = Encoder_walk(state_dim, hidden_dim, observable_dim)
         self.decoder = Decoder(observable_dim, state_dim)
         self.state_dim = state_dim
         self.hidden_dim = hidden_dim
         self.observable_dim = observable_dim
-        # self.K = torch.randn(hidden_dim+state_dim, hidden_dim+state_dim)  
         self.K = torch.randn(observable_dim, observable_dim).to(device)  
     
     def forward(self, x):
 
         z = self.encoder(x)  
 
-        if self.K is not None:
-            z_next = torch.matmul(z, self.K.T)  # Apply computed Koopman operator
-        else:
-            z_next = z  
+        # if self.K is not None:
+        z_next = torch.matmul(z, self.K.T)  # Apply computed Koopman operator
+        # else:
+        #     z_next = z  
 
         y_hat = self.decoder(z_next)
         x_hat = self.decoder(z)  
         return x_hat, z, y_hat
         
     def compute_koopman_operator(self, latent_X, latent_Y,device):
-        # print(latent_X.shape, latent_Y.shape)
-        latent_X = latent_X.view(-1, latent_X.size(-1))  # [N, d]
-        latent_Y = latent_Y.view(-1, latent_Y.size(-1))  # [N, d]
+        # # print(latent_X.shape, latent_Y.shape)
+        # latent_X = latent_X.view(-1, latent_X.size(-1))  # [N, d]
+        # latent_Y = latent_Y.view(-1, latent_Y.size(-1))  # [N, d]
 
-        X_pseudo_inv = torch.linalg.pinv(latent_X.T)  # Compute pseudo-inverse of latent_X
-        # self.K = torch.matmul(latent_Y.T, X_pseudo_inv.T).to(device)  # K = Y * X^+
-        self.K = (latent_Y.T @ X_pseudo_inv).to(device)
-        # print(self.K.shape)
+        # X_pseudo_inv = torch.linalg.pinv(latent_X.T)  # Compute pseudo-inverse of latent_X
+        # # self.K = torch.matmul(latent_Y.T, X_pseudo_inv.T).to(device)  # K = Y * X^+
+        # self.K = (latent_Y.T @ X_pseudo_inv).to(device)
+        # # print(self.K.shape)
+
+        latent_X = latent_X.view(-1, latent_X.size(-1))  # [N, d]
+        latent_X = latent_X.T # [d, N]
+        latent_Y = latent_Y.view(-1, latent_Y.size(-1))  # [N, d]
+        latent_Y = latent_Y.T
+        # X_pseudo_inv = torch.linalg.pinv(latent_X.T)  # Compute pseudo-inverse of latent_X
+        # self.K = latent_Y.T @ X_pseudo_inv
+        self.K = latent_Y @ torch.linalg.pinv(latent_X, rcond=1e-5)
