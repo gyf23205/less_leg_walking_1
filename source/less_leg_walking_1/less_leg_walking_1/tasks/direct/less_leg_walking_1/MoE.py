62a125de19aee93083290f991e3f9a97c18c7290
netakoi <choi774@purdue.edu>
2026-01-14 16:05:35 -0500

    bak

--- source/less_leg_walking_1/less_leg_walking_1/tasks/direct/less_leg_walking_1/MoE.py
+++ source/less_leg_walking_1/less_leg_walking_1/tasks/direct/less_leg_walking_1/MoE.py
@@ -5,13 +5,13 @@ from isaaclab_rl.rsl_rl import RslRlPpoActorCriticCfg
 class MoECfg(RslRlPpoActorCriticCfg):
     """Configuration for the custom MoE policy."""
     padded_dim: int = 256
-    observable_dim: int = 16
+    observable_dim: int = 64
     actor_hidden_dims: list[int] = [512, 256, 128]
     # actor_hidden_dims: list[int] = [256, 128, 64]
     critic_hidden_dims: list[int] = [512, 256, 128]
     # kae_path: str = "/home/yifan/git/less_leg_walking_1/source/less_leg_walking_1/less_leg_walking_1/tasks/direct/less_leg_walking_1/KAE_original_range.pth"
     # kae_path: str = "/home/joonwon/github/Koopman_decompose_ext/KAE/waypoints/new_bound2.pth"
-    kae_path: str = "/home/joonwon/github/Koopman_decompose_ext/KAE/waypoints/ForMOE_p1_pad256_obv16.pth"
+    kae_path: str = "/home/joonwon/github/Koopman_decompose_ext/KAE/waypoints/ForMOE_p1_pad256_obv64.pth"
     device: str = "cuda"
     n_experts: int = 1
     p: int = 1
@@ -206,16 +206,26 @@ class MoEActorCritic(ActorCritic):
         assert temp[1]==235, "observation is not 235 dim"
 
         padded_obs = self._prep_obs(obs)
+
+
+        #######################################
         with torch.no_grad():  
             _, latent_z, _ = self.kae(padded_obs)
 
-            latent_z = latent_z.detach()
+            # latent_z = latent_z.detach()
             if latent_z.ndim == 1:
                 latent_z = latent_z.unsqueeze(0)
 
             experts_outputs = get_experts_outputs(self.kae, latent_z, self.p, self.act_dim)# (Batch, observable_dim, act_dim)
             extended_experts_outputs = extend_experts_outputs(experts_outputs, self.act_dim)
 
+        # _, latent_z, _ = self.kae(padded_obs)  # KAE params still frozen
+        # if latent_z.ndim == 1:
+        #     latent_z = latent_z.unsqueeze(0)
+
+        # experts_outputs = get_experts_outputs(self.kae, latent_z, self.p, self.act_dim)
+        # extended_experts_outputs = extend_experts_outputs(experts_outputs, self.act_dim)
+        #######################################
 
         # print(experts_outputs.shape)
         # print(extended_experts_outputs.size())
--- source/less_leg_walking_1/less_leg_walking_1/tasks/direct/less_leg_walking_1/agents/rsl_rl_ppo_cfg.py
+++ source/less_leg_walking_1/less_leg_walking_1/tasks/direct/less_leg_walking_1/agents/rsl_rl_ppo_cfg.py
@@ -12,7 +12,7 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 # from ..res_net import ResCfg, ResActorCritic  # Import ResActorCritic
 
 # USE THIS FOR MoE
-# from ..MoE import MoECfg, MoEActorCritic  # Import both
+from ..MoE import MoECfg, MoEActorCritic  # Import both
 
 # Comment all for scratch
 # ############################################
@@ -28,12 +28,14 @@ class LessLegWalkingFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     # experiment_name = "MoE16_1e4rl_less_leg_walking_flat"
     # experiment_name = "MoE12_noext_less_leg_walking_flat"
     # experiment_name = "MoE12_1e4rl_less_leg_walking_flat"
-    # experiment_name = "MoE32_less_leg_walking_flat"
+    
     # experiment_name = "MoE12_less_leg_walking_flat"
 
-    # experiment_name = "MoE16_less_leg_walking_flat"
+    experiment_name = "MoE16_less_leg_walking_flat"
+    # experiment_name = "MoE32_less_leg_walking_flat"
+    # experiment_name = "MoE64_less_leg_walking_flat"
     # experiment_name = "Residual_less_leg_walking_flat"
-    experiment_name = "Nominal_less_leg_walking_flat"
+    # experiment_name = "Nominal_less_leg_walking_flat"
     empirical_normalization = False
 
     ############################################
@@ -41,15 +43,15 @@ class LessLegWalkingFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     # policy = ResCfg()
 
     # USE THIS FOR MoE
-    # policy = MoECfg()
+    policy = MoECfg()
 
-    # # USE THIS FOR Scratch
-    policy = RslRlPpoActorCriticCfg(
-        init_noise_std=1.0,
-        actor_hidden_dims=[512, 256, 128],
-        critic_hidden_dims=[512, 256, 128],
-        activation="elu",
-    )
+    # # # USE THIS FOR Scratch
+    # policy = RslRlPpoActorCriticCfg(
+    #     init_noise_std=1.0,
+    #     actor_hidden_dims=[512, 256, 128],
+    #     critic_hidden_dims=[512, 256, 128],
+    #     activation="elu",
+    # )
     ############################################
 
     algorithm = RslRlPpoAlgorithmCfg(
